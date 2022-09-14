7e2110da126d2c1bea2d35c27437f1675f2a2455
raulkele <raulkeleblk@gmail.com>
2022-09-14 12:27:04 +0000

    Deploying to gh-pages from @ raulkele/site-zot@7df86516e90dd80ca11b86f633324b21e8f8aaf8 🚀

--- _next/static/a4xUs_B1eKEt60G4qyXUq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/129d2484dd9b7a88.css","static/chunks/pages/index-1699a95675824797.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
